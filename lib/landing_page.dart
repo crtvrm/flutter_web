46e781052ad440c1b8131d329c7f4690b093c1ab
CRTVRM <crtvrm@gmail.com>
2023-04-04 12:46:32 +0700

    initial commit

---
+++ lib/landing_page.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget navItem({required String title, required int index}) {
+      return InkWell(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              style: GoogleFonts.poppins(
+                fontSize: 18,
+                color: const Color(0xff1D1E3C),
+                fontWeight:
+                    index == selectedIndex ? FontWeight.w500 : FontWeight.w300,
+              ),
+            ),
+            Container(
+              width: 66,
+              height: 2,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: index == selectedIndex
+                    ? const Color(0xffFE998D)
+                    : Colors.transparent,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'images/background.png',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.fill,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 100,
+              vertical: 30,
+            ),
+            child: Column(
+              children: [
+                // NOTE : NAVIGATION
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      'images/logo.png',
+                      width: 72,
+                      height: 40,
+                    ),
+                    Row(
+                      children: [
+                        navItem(
+                          title: 'Guides',
+                          index: 0,
+                        ),
+                        const SizedBox(
+                          width: 50,
+                        ),
+                        navItem(
+                          title: 'Pricing',
+                          index: 1,
+                        ),
+                        const SizedBox(
+                          width: 50,
+                        ),
+                        navItem(
+                          title: 'Team',
+                          index: 2,
+                        ),
+                        const SizedBox(
+                          width: 50,
+                        ),
+                        navItem(
+                          title: 'Stories',
+                          index: 3,
+                        ),
+                      ],
+                    ),
+                    Image.asset(
+                      'images/btn_account.png',
+                      width: 163,
+                      height: 53,
+                    ),
+                  ],
+                ),
+
+                // NOTE : CONTENT
+                const SizedBox(
+                  height: 76,
+                ),
+                Image.asset(
+                  'images/hero_big_image.png',
+                  height: 550,
+                ),
+
+                //NOTE : SCROLL TEXT
+                const SizedBox(
+                  height: 84,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'images/btn_scroll.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                    const SizedBox(
+                      width: 13,
+                    ),
+                    Text(
+                      'Scroll to Learn More',
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                        color: const Color(0xff000000),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
